a73e163ce858f30b61e141677a62a0e59adb8881
Sabrina Belloni <sabrina82belloni@gmail.com>
2024-09-08 18:39:32 +0000

    tidy up comments

--- run.py
+++ run.py
@@ -5,11 +5,13 @@
 
 
 def gather_user_name():
+
     """
     collect the name of the customer as variable and 
     add validation steps for avoid accidental insertion of number 
     add valildation also for none value
     """
+
     while True:
         try:
             user_name = input("enter your first name:\n")
@@ -24,11 +26,13 @@ def gather_user_name():
         
 
 def daily_steps(username):
+
     """
     collect the average number of daily steps
     add validation for numbers only
 
     """
+
     while True:
         try:
             number_of_steps = int(input("Enter the average number of your daily steps:\n"))
@@ -42,27 +46,36 @@ def daily_steps(username):
     
 
 def kcal_burned(username, number_of_steps):
+
     """
     define how many calories were burned so far
     Generally speaking, walking 10km will burn around 500-700 (i will use 600 
     as average of the 2 values)
     calories for an average person this function will use this for calculate
     the kcal already burnt
+    calory rounded to the integer (no decimal number)
     """
-    #single_step = 10000 / 600 #600 kcal average for 10k km, single step calculated
-    daily_kcal_burned = round(number_of_steps * 0.06)
+
+    single_step = 600 / 10000 #600 kcal average for 10k km, single step calculated
+    daily_kcal_burned = round(number_of_steps * single_step)
+
     if daily_kcal_burned > 50:
         print(f"Great news {username}, you already burned {daily_kcal_burned} Kcal today! keep going!\n")
     else:
         print(f"Hey {username}, you burned {daily_kcal_burned} kcal so far, but you can do better!\n")
 
-
+        
 
 print("Welcome to our StepCounter App\n")
 print("This app was created for keep track of daily steps for weightloss\n")
-print("Let's start!")
+print("Let's start!\n")
+
 
 def main():
+    """
+    main function that run all the program function
+
+    """
    
     username = gather_user_name()
     number_of_steps = daily_steps(username)
