6b575004c7aaf35d8436ec3b108534639e0aab07
Sabrina Belloni <sabrina82belloni@gmail.com>
2024-09-08 19:18:15 +0000

    correct document as per pep8

--- run.py
+++ run.py
@@ -3,9 +3,7 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 
-
 def gather_user_name():
-
     """
     collect the name of the customer as variable and 
     add validation steps for avoid accidental insertion of number 
@@ -23,14 +21,12 @@ def gather_user_name():
                 print("Invalid username. Insert your first name only\n")
         except ValueError:
             print("Please try again")
-        
+
 
 def daily_steps(username):
-
     """
     collect the average number of daily steps
     add validation for numbers only
-
     """
 
     while True:
@@ -43,10 +39,9 @@ def daily_steps(username):
                 print("invalid number. try again\n")
         except ValueError:
             print("only numbers accepted.\n")
-    
+
 
 def kcal_burned(username, number_of_steps):
-
     """
     define how many calories were burned so far
     Generally speaking, walking 10km will burn around 500-700 (i will use 600 
@@ -56,7 +51,7 @@ def kcal_burned(username, number_of_steps):
     calory rounded to the integer (no decimal number)
     """
 
-    single_step = 600 / 10000 #600 kcal average for 10k km, single step calculated
+    single_step = 600 / 10000  # 600 kcal average for 10k km, single step calculated
     daily_kcal_burned = round(number_of_steps * single_step)
 
     if daily_kcal_burned > 50:
@@ -64,7 +59,6 @@ def kcal_burned(username, number_of_steps):
     else:
         print(f"Hey {username}, you burned {daily_kcal_burned} kcal so far, but you can do better!\n")
 
-        
 
 print("Welcome to our StepCounter App\n")
 print("This app was created for keep track of daily steps for weightloss\n")
@@ -74,9 +68,8 @@ print("Let's start!\n")
 def main():
     """
     main function that run all the program function
-
     """
-   
+
     username = gather_user_name()
     number_of_steps = daily_steps(username)
     daily_kcal_burned = kcal_burned(username, number_of_steps)
